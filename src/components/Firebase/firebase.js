a117a21ee9eb562200a5d0cca162cc44c6ace947
macmorning <sylvain.yvon@gmail.com>
2018-12-01 22:27:55 +0100

    finished session handling

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,13 +1,14 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
-  apiKey: process.env.MBS_API_KEY,
-  authDomain: process.env.MBS_AUTH_DOMAIN,
-  databaseURL: process.env.MBS_DATABASE_URL,
-  projectId: process.env.MBS_PROJECT_ID,
-  storageBucket: process.env.MBS_STORAGE_BUCKET,
-  messagingSenderId: process.env.MBS_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
@@ -15,6 +16,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
   // *** Auth API ***
 
@@ -30,6 +32,10 @@ class Firebase {
 
     doPasswordUpdate = password =>
       this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+    user = uid => this.db.ref(`users/${uid}`);
+    users = () => this.db.ref('users');
 }
 
 export default Firebase;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpPage = () => (
@@ -18,20 +19,45 @@ const INITIAL_STATE = {
   error: null,
 };
 
-class SignUpForm extends Component {
+class SignUpFormBase extends Component {
   constructor(props) {
     super(props);
+
     this.state = { ...INITIAL_STATE };
   }
 
   onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
 
-  }
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        // Create a user in your Firebase realtime database
+        this.props.firebase
+          .user(authUser.user.uid)
+          .set({
+            username,
+            email,
+          })
+          .then(() => {
+            this.setState({ ...INITIAL_STATE });
+            this.props.history.push(ROUTES.HOME);
+          })
+          .catch(error => {
+            this.setState({ error });
+          });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
 
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-  
+
   render() {
     const {
       username,
@@ -40,6 +66,12 @@ class SignUpForm extends Component {
       passwordTwo,
       error,
     } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
 
     return (
       <form onSubmit={this.onSubmit}>
@@ -71,9 +103,12 @@ class SignUpForm extends Component {
           type="password"
           placeholder="Confirm Password"
         />
-        <button type="submit">Sign Up</button>
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
 
-        {error && <p>{error.message}</p>}      </form>
+        {error && <p>{error.message}</p>}
+      </form>
     );
   }
 }
@@ -83,6 +118,8 @@ const SignUpLink = () => (
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
+
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
 export default SignUpPage;
 
