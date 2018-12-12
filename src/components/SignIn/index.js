066adb521d2b1d72a4ac5010337c7aac7d74079e
macmorning <sylvain.yvon@gmail.com>
2018-12-12 18:51:33 +0100

    added material to signin

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -6,17 +6,56 @@ import { SignUpLink } from '../SignUp';
 import { PasswordForgetLink } from '../PasswordForget';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 
-const SignInPage = () => (
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+function SignInPage(props) {
+  return (
   <div>
-    <h1>SignIn</h1>
-    <SignInForm />
+    <SignInForm  {...props}></SignInForm>
     <PasswordForgetLink />
     <SignUpLink />
   </div>
-);
+  );
+}
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  }
+});
 
 const INITIAL_STATE = {
   email: '',
@@ -27,7 +66,7 @@ const INITIAL_STATE = {
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
-
+    console.log(this.props);
     this.state = { ...INITIAL_STATE };
   }
 
@@ -53,40 +92,60 @@ class SignInFormBase extends Component {
 
   render() {
     const { email, password, error } = this.state;
-
+    const { classes } = this.props;
+    console.log(this.props);
     const isInvalid = password === '' || email === '';
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <TextField
-          name="email"
-          label="Email Address"
-          value={email}
-          onChange={this.onChange}
-          margin="normal"
-        />
-        <TextField
-          name="password"
-          label="Password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          margin="normal"
-        />
-        <Button variant="contained" color="primary" type="submit" disabled={isInvalid}>
-        Sign In
-        </Button>
+      <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={this.onSubmit}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus onChange={this.onChange}/>
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input name="password" type="password" id="password" autoComplete="current-password" onChange={this.onChange}/>
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={isInvalid}
+          >
+            Sign in
+          </Button>
+        </form>
         {error && <p>{error.message}</p>}
-      </form>
+      </Paper>
+</main>
     );
   }
 }
+
+SignInFormBase.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const SignInForm = compose(
   withRouter,
   withFirebase,
 )(SignInFormBase);
 
-export default SignInPage;
-
 export { SignInForm };
+export default withStyles(styles)(SignInPage);
+
