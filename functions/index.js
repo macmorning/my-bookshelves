08ea28f19f4749bb3b6a7fe9557f66777fa747a6
macmorning <sylvain.yvon@gmail.com>
2019-01-10 18:23:27 +0100

    firebase function  works :)

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,55 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'),
+      https = require('https'),
+      parseString = require('xml2js').parseString,
+      request =require('request-promise');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const lookupConfig = {
+  // set using firebase functions:config:set libthing.key=...
+  libthing_api_key: functions.config().libthing.key || false,
+  libthing_baseurl: "https://www.librarything.com/services/rest/1.1/?method=librarything.ck.getwork&isbn=@@isbn@@&apikey=@@apikey@@"
+};
+    
+exports.fetchBookInformations = functions.database.ref('/bd/{user}/{ref}/needLookup').onWrite((snapshot, context) => { 
+    // Grab the current value of what was written to the Realtime Database.
+    let isbn = context.params.ref;
+    console.log('onWrite triggered on book ' + isbn + ', needLookup = ' + snapshot.after.val());
+    if (!snapshot.after.exists() || snapshot.after.val() !== 1) {
+        return null;
+    }
 
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.testFunction = functions.database.ref('/bd/{user}/{ref}')
-    .onWrite((snapshot, context) => { 
-      // Grab the current value of what was written to the Realtime Database.
-      console.log('onWrite triggered');
-      console.log(JSON.stringify(context));
-      console.log(JSON.stringify(snapshot));
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      // return snapshot.ref.parent.child('uppercase').set(uppercase);
+    let url = lookupConfig.libthing_baseurl.replace("@@apikey@@",lookupConfig.libthing_api_key).replace("@@isbn@@",isbn);
+
+
+    https.get(url, res => {
+        res.setEncoding("utf8");
+        let response = "";
+        res.on("data", data => {
+            response += data;
+        });
+        res.on("end", () => {
+           response = parseString(response, function(err, result) {
+                var dataRef = snapshot.after.ref.parent;
+
+                if (result.response.ltml !== undefined && result.response.ltml[0] !== undefined && result.response.ltml[0].item[0] !== undefined) {
+                    let book = result.response.ltml[0].item[0];
+                    return dataRef.update({
+                        title: (book.title.length > 0 ? book.title[0]:"?"),
+                        author: (book.author.length > 0 ? book.author[0]["_"]:""),
+                        imageURL: "",
+                        detailsURL: (book.url.length > 0 ? book.url[0]:""),
+                        published: "",
+                        publisher: "",
+                        needLookup: 0
+                    });
+                } else {
+                    console.log(" ... not found!");
+                    return dataRef.update({
+                        title: "not found!",
+                        needLookup: 0
+                    });
+                }
+           });
+        });
     });
+  }
+);
